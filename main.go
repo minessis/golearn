a23b3c6d7e8c714b43bc2621bfbfc27c8ddb3cc9
minessis <ralph.lewis.ii@gmail.com>
2018-09-20 14:42:10 -0400

    [ADD] created struct example

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+// structs are used widely throughout Golang.
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+// When we see the star--asterisk--operator in front of a
+// type, it means we're working with the pointer to the
+// type, and in this case, a pointer to a person
+func (p *person) updateName(newFirstName string) {
+	// When we see the asterisk operator in front of a pointer
+	// it takes the pointer and turns it back into the value
+	// the pointer is referencing
+	(*p).firstName = newFirstName
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	// jimPointer := &jim
+	jim.updateName("Jimmy")
+	jim.print()
 }
