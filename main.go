fcf29b0977eaed983b46b349837e34b8830348ec
minessis <ralph.lewis.ii@gmail.com>
2018-09-20 15:08:23 -0400

    [EDIT] created branch maps

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	// literal map syntax
+	// create a map where:
+	// all the keys are strings, and
+	// all the values are maps
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
+
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
