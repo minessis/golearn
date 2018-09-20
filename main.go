bcc4bd4121a6b2cfcf1d1035e250f403445625eb
minessis <ralph.lewis.ii@gmail.com>
2018-09-20 15:50:04 -0400

    [ADD] interfaces tutorial

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import "fmt"
+
+type englishBot struct{} // concrete type
+type spanishBot struct{} // concrete type
+type bot interface {     // interface type
+	getGreeting() string
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola"
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello"
 }
