77964f5db503fd322871176751fa767281d38b79
minessis <ralph.lewis.ii@gmail.com>
2018-09-20 20:46:23 -0400

    [ADD] created channel example

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	links := []string{
+		"https://facebook.com",
+		"https://google.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		// since go is a pass by value language
+		// we're going to send in the link
+		// value as a property, so the child routine
+		// has a copied value of the link
+		go func(link string) { // `function literal`
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
 }
